a7a43c65052f211ad3997ee28d41c9a9b51ca1db
valincius <john@valincius.dev>
2022-07-26 16:49:03 +0000

    Deploying to gh-pages from @ valincius/BlazorScheduler@f36c54a4333886f5d68357cc404aba5e1b6271ea 🚀

--- _content/BlazorScheduler/js/scripts.js
+++ _content/BlazorScheduler/js/scripts.js
@@ -22,18 +22,29 @@ window.BlazorScheduler.attachSchedulerMouseEventsHandler = objRef => {
     window.BlazorScheduler.mouseMoveListener = window.BlazorScheduler.throttle(e => {
         let distances = [];
 
+        let mouseX = parseInt(e.clientX);
+        let mouseY = parseInt(e.clientY);
+
         let elements = Array.from(document.querySelectorAll('.day'));
-        elements.forEach(elem => {
+        for (elem of elements) {
             let rect = elem.getBoundingClientRect();
+
+            if (rect.x <= mouseX && (rect.x + rect.width) >= mouseX
+                && rect.y <= mouseY && (rect.y + rect.height) >= mouseY) {
+                objRef.invokeMethodAsync('OnMouseMove', elem.dataset.date);
+                return;
+            }
+
             let x = rect.x + rect.width / 2;
             let y = rect.y + rect.height / 2;
-            let distance = Math.hypot(x - parseInt(e.clientX), y - parseInt(e.clientY));
-            distances.push(parseInt(distance));
-        });
+            let distance = Math.hypot(x - mouseX, y - mouseY);
+            distances.push({ elem, dist: parseInt(distance) });
+        }
 
-        let closestLinkIndex = distances.indexOf(Math.min(...distances));
-        if (closestLinkIndex >= 0) {
-            objRef.invokeMethodAsync('OnMouseMove', elements[closestLinkIndex].dataset.date);
+        distances.sort((a, b) => a.dist - b.dist);
+        let closest = distances[0];
+        if (closest !== null) {
+            objRef.invokeMethodAsync('OnMouseMove', closest.elem.dataset.date);
         }
     }, 50);
 
